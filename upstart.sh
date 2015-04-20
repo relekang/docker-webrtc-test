ef10531d7b813c80169a0613d4d469cb350824f2
Rolf Erik Lekang <me@rolflekang.com>
2015-04-20 06:23:22 +0200

    Move chrome dockerfile within a chrome folder

--- upstart.sh
+++ upstart.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-export URL=http://aberforth.lkng.me
-export LOG_FILE=/var/log/chrome-headless.log
+export LOG_FILE=/var/log/browser-xvfb.log
 export DISPLAY=:10
 
 Xvfb :10 -screen 0 1366x768x24 -ac +extension RANDR & > ${LOG_FILE} \
